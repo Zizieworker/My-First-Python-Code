d446d61765001dcaee552c45dfd2605fbe9a11d0
mochfauzi573 <mochfauzi573@gmail.com>
2022-01-10 11:12:10 +0700

    my first code with python

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
 """
-ini adalah code demo dengan python
+semua sintaksis bahasa pemograman :
+1.Sekuensial : Langkah berurutan
+2.Percabangan : langkah melompat jika kondisi terpenuhi
+3.Perulanggan : mengulang langkah berkali kali hingga kondisi terpnuhi
 """
-print("Hello Giyu Tamioka")
-print("My Name is Tamando Tanjiro")
+#sekuensial
+
+print("ibu berkata ,'Pergi ke toko '")
+print("Budi menjawab , 'baik apa yang bisa saya lakukan '")
+print("Ibu menjawab  , 'belikan satu botol susu dan jika ada telor beli 6  '")
+print("Maka budi berangkat ke toko ")
+print("Maka budi mulai berbelanja ")
